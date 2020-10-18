c1b3ecfa1b300be8333f69ab901b397ceba22a6a
ganawa <ghana.afani@gmail.com>
2020-10-18 14:58:01 +0700

    [update] no 2

--- logic/2.js
+++ logic/2.js
@@ -1,7 +1,7 @@
 const recurString = (string) => {
    let splitString = string.split('');
-   let searchRecur = splitString.filter((value, index) => value === value[index]);
-   console.log(`Karakter pertama yang berulang adalah ${searchRecur.toString()}`)
+   let searchRecur = splitString.find((value, index, arr) => arr.lastIndexOf(value) !== index);
+   console.log(`Karakter pertama yang berulang adalah ${searchRecur}`)
 }
 
 recurString("BCABA");
